aa9bf96b80cfe2f17d329eae075e5ea910d3292f
Simona Vasileva <149930250+SimoneWas@users.noreply.github.com>
2023-12-06 11:08:39 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+console.log("wanna be web developer")
+
+
+
+// exercises from presentation
+let firstSide = "3";
+let secondSide = "8";
+console.log(firstSide * secondSide);
+
+let r = 10;
+let p = 3.14;
+let areaCircle = p * r ** 2;
+console.log(areaCircle);
+
+
+/**
+ * Write a JavaScript program that will calculate the price of 30 Phones, where the price of one phone is $119.95 and the tax rate is 5%.
+ * 1.write down phone price without the tax
+ * 2.write the quantity of phones needed to calculate
+ * 3.write down the tax percentage
+ * 4.let the console.log calculate 5% of the price of one phone
+ * 5.let let console.log calculate the price without tax of one phone, the price minus the result of taxFromPrice
+ * 6.let console log multiply the priceWithoutTax with quantity of phones to have the final result printed in browser
+ */
+
+let price = 119.95;
+let phones = 30;
+let taxPercent = 5 %
+
+
+console.log(taxFromPrice = 119.95 / 100 * 5)
+console.log(priceWithoutTax = price - taxFromPrice)
+console.log(priceWithoutTax * phones)
+
